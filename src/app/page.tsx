0eaaace87f0805bc062b4d6471abc98455cd112c
Updesh Srivastava <updesh.94@gmail.com>
2024-04-22 09:32:25 +0000

    Added SearchBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function Home() {
   return (
@@ -29,6 +34,40 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      <Container>
+        <Row className={styles.search}>
+          <InputGroup
+            style={{ width: "1000px" }}
+            className="mx-auto px-2 input-group-lg"
+          >
+            <FormControl
+              list="datalistOptions"
+              id="bookListSearch"
+              placeholder="Search Library Books..."
+            />
+            <datalist id="datalistOptions">
+              <option value="Kent's Homeopathy" />
+              <option value="Germany" />
+              <option value="India" />
+              <option value="Homeopathy for Life" />
+              <option value="Chicago" />
+            </datalist>
+            <Button className="btn btn-primary" variant="primary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
+            </Button>
+          </InputGroup>
+        </Row>
+      </Container>
 
       <div className={styles.grid}>
         <Link className={styles.card} href="/about">
